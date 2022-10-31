92ef5ec76622a272e75ed7eaba00d1c37182a0cb
Tolgahan <tolga_han_345@hotmail.com>
2022-10-31 16:24:49 +0300

    No commit message

---
+++ src/components/Celebrate.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <h1>Tebrikler Bildiniz!!!</h1>
+        <button @click="newGame" >Yeni Oyun</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods : {
+        newGame(){
+            this.$emit("activeComponentEvent","app-game-cards")
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+.container h1 {
+    text-align: center;
+    font-size: 300%;
+    color: darkgreen;
+    opacity: .5;
+}
+.container button{
+    cursor: pointer;
+    width: 200px;
+    background-color: #fff;
+    color: darkred;
+    border: 1px solid darkgreen;
+    height: 50px;
+    font-size: 15px;
+    border-radius: 5px;
+    transition: all .5s;
+    outline: none;
+}
+.container button:hover{
+    background-color: darkgreen;
+    color: #fff;
+    transition: all .5s;
+}
+</style>>
+</style>
